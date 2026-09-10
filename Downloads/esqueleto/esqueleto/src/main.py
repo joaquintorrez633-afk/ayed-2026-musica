5b528389b98fc8d49bf9b7ce7fde4b76e7c222a3
benjalmolina <molina05benjamin@gmail.com>
2026-09-10 00:44:32 -0300

    Change import from listar_catalogo in main.py

--- Downloads/esqueleto/esqueleto/src/main.py
+++ Downloads/esqueleto/esqueleto/src/main.py
@@ -1,6 +1,5 @@
 from src.config import TEMA
-from src.dominio.musica import listar_catalogo
-
+from src.dominio.cancion import listar_catalogo
 TEMAS = {
     "pokedex": "Pokédex",
     "recetario": "Recetario",
@@ -39,7 +38,9 @@ def main():
         opcion = input("> ").strip()
         if opcion == "0":
             print("Chau.")
-        elif opcion in {"1", "2", "3", "4", "5", "6", "7", "8", "9"}:
+        elif opcion == "1":
+            listar_catalogo()
+        elif opcion in {"2", "3", "4", "5", "6", "7", "8", "9"}:
             pendiente()
         else:
             print("Opción inválida.")
