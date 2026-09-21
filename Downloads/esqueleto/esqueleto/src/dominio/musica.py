34dc86b70daee8d355941f57af3d69a61daac907
joaquintorrez633-afk <joaquintorrez633@gmail.com>
2026-09-21 00:40:13 -0300

    creacion de canciones con poo, solo queda biblioteca

--- Downloads/esqueleto/esqueleto/src/dominio/musica.py
+++ Downloads/esqueleto/esqueleto/src/dominio/musica.py
@@ -1,5 +1,5 @@
 catalogo= [
-    {'id': 1, 'titulo': 'Bohemian Rhapsody (Live Aid)', 'artista': 'Queen', 'genero': 'Rock', 'anio': 1985, 'duracion': '6:'},
+    {'id': 1, 'titulo': 'Bohemian Rhapsody (Live Aid)', 'artista': 'Queen', 'genero': 'Rock', 'anio': 1985, 'duracion': '6:00'},
     {'id': 2, 'titulo': 'JiJi','artista': 'Indio Solari', 'genero':'rock', 'anio': 1986, 'duracion': '5:41'},
     {'id': 3, 'titulo': 'Nunca Quise', 'artista': 'Intoxicados', 'genero':'rock', 'anio':2005, 'duracion': '4:21' }
 ]
